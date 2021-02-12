0ec1d86dfe1c52799f32991240e05f2ad18d8966
Christine Chee <christinechee88@hotmail.com>
2021-02-12 11:30:56 +0000

    Add solution to exercise 4

--- extract_method/4.rb
+++ extract_method/4.rb
@@ -4,12 +4,13 @@ class Mail
     @fancy = fancy
   end
 
+  def body
+    base_body = "Welcome to MyProduct"
+    body = @fancy ? "#{ base_body }, fancy person!" : base_body
+  end
+
   def send_message
     mail_server = MailServer.connect("http://mixmax.com", api_key: "f20506xx808c")
-    base_body = "Welcome to MyProduct"
-
-    body = @fancy ? "#{ base_body }, fancy person!" : base_body
-
-    mail_server.send_message({ to: @email, body: base_body })
+    mail_server.send_message({ to: @email, body: body })
   end
 end
