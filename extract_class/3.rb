d5e9506e1add2eba3c964695896e6162cb3036a1
Christine Chee <christinechee88@hotmail.com>
2021-02-12 12:10:36 +0000

    Add solution to exercise 3

--- extract_class/3.rb
+++ extract_class/3.rb
@@ -4,16 +4,18 @@ class BrowserPageChooser
     safari_small_message = "You are using the Safari browser in a small window."
     not_safari_or_big_message = "You are not using the Safari browser or have a big window."
 
-    safari?(browser) && small?(window) ? safari_small_message : not_safari_or_big_message
+    Browser.new.safari?(browser) && Window.new.small?(window) ? safari_small_message : not_safari_or_big_message
   end
+end
 
-  private
-
-  def safari?(browser)
-    browser.type == "Safari"
-  end
-
+class Window
   def small?(window)
     window.size < "768px"
+  end
+end
+
+class Browser
+  def safari?(browser)
+    browser.type == "Safari"
   end
 end
