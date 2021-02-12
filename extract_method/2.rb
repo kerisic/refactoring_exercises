bbd6bcba73d4453b34ac6ac3020a2d9a2365c7cc
Christine Chee <christinechee88@hotmail.com>
2021-02-12 10:53:09 +0000

    Add solution to exercise 2

--- extract_method/2.rb
+++ extract_method/2.rb
@@ -1,7 +1,10 @@
+def is_safari?(browser)
+  browser.type == "Safari"
+end
+
 def tell_browser_type(browser)
-  browser_type_is_safari = browser.type == "Safari"
   safari_message = "You are using the Safari browser."
   not_safari_message = "You are using a non-Safari browser."
 
-  browser_type_is_safari ? puts safari_message : puts not_safari_message
+  is_safari?(browser) ? puts safari_message : puts not_safari_message
 end
